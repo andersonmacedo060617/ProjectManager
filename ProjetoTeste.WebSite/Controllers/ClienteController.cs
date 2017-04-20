7a8f146b50211be6663e9c27c6d105e38ebebaf6
Anderson Macedo <anderson.macedo@thomsonreuters.com>
2017-04-20 18:10:37 -0300

    - Iniciando CRUD

---
+++ ProjetoTeste.WebSite/Controllers/ClienteController.cs
@@ -0,0 +1,36 @@
+﻿using ProjetoTeste.NH.Config;
+using ProjetoTeste.NH.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjetoTeste.WebSite.Controllers
+{
+    
+    public class ClienteController : Controller
+    {
+        // GET: Cliente
+        [Authorize (Roles ="Usuario, Administrador")]
+        public ActionResult Index()
+        {
+            var clientes = ConfigDB.Instance.ClienteRepository.BuscarTodos();
+            return View(clientes);
+        }
+
+        [Authorize(Roles = "Usuario, Administrador")]
+        public ActionResult Novo()
+        {
+            Cliente cliente = new Cliente();
+
+            if(TempData["ClienteReturn"] != null)
+            {
+                cliente = TempData["ClienteReturn"] as Cliente;
+            }
+
+            return View(cliente);
+        }
+    }
+    
+}
